1eb8c1509396790969f0c6661e5a54fc32c499a2
Mikkel <mbek@itu.dk>
2024-02-20 13:32:40 +0100

    asd

---
+++ Assignment4/MultiSet.fs
@@ -0,0 +1,42 @@
+module MultiSet
+
+    type MultiSet = unit // replace with your type
+
+
+    let empty _ = ()
+
+    let isEmpty _ = true
+
+    let size _ = 0u
+    
+    let contains _ _ = true
+
+    let numItems _ _ = 0u
+
+    let add _ _ _ = ()
+
+    let addSingle _ _ = ()
+    
+    let remove _ _ _ = ()
+
+    let removeSingle _ _ = ()
+
+
+    let fold _ x _ = x 
+    let foldBack _ _ x = x
+    
+    let ofList _ = ()
+    let toList _ = []
+
+
+    let map _ _ = ()
+
+
+    let union _ _ = ()
+    let sum _ _ = ()
+    
+    let subtract _ _ = ()
+    
+    let intersection _ _ = ()
+       
+    
